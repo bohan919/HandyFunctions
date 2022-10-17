2d2a29d8ce57f9fcae3451fee6fd54a1efcee5b1
Bohan Peng <pengbohan@hotmail.com>
2022-10-17 17:32:25 +0100

    minor update

--- run.py
+++ run.py
@@ -2,17 +2,26 @@
 import os
 from strucAnal import TOStruc
 
-folder = './vfProcessed/'
-files = os.listdir(folder)
-output = open(folder+'output.txt', 'a')
+folder = './combinedProcessed/'
+# files = os.listdir(folder)
+files = [file for file in os.listdir(folder) if file.endswith('.png')]
+output = open(folder+'outputThres.txt', 'a')
 
 for file in files:
 
     PATH = folder+file
-    test1 = TOStruc(PATH)
+    test1 = TOStruc(PATH,1)
 
     compliance = test1.compliance()
     ce = test1.strainEnergy()
     vf = test1.vf()
 
     output.write("%s \t %s \t %s \n" %(file,vf, compliance))
+
+##### TESTS
+# testPath = 'test1.png'
+# test1 = TOStruc(testPath)
+# test1Thres = TOStruc(testPath, 1)
+
+# print(test1.compliance())
+# print(test1Thres.compliance())
